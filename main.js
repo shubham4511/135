d739567af16793b41a0665adedb16d0456ef54e2
shubham4511 <89202701+shubham4511@users.noreply.github.com>
2022-10-17 19:18:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-video="";
-status="";
-function preload(){
-    video=createVideo();
+Status="";
+input_text="";
+
+function setup () {
+    canvas=createCanvas(400,380);
+    canvas.center();
+    canvas.position(680,250);
+    video=createCapture(VIDEO);
+    video.size(400,380);
     video.hide();
 }
-function setup () {
-    canvas=createCanvas(480,380);
-    canvas.center();
+function start(){
+    objectDetector= ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+    input_text=document.getElementById("input_id").value;
+
+}
+function modelLoaded(){
+    console.log("model_loaded");
+    Status=true;
+}
+function drew(){
+    image(video,0,0,400,380);
 }
